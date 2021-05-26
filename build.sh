e5ccb556df556bcf9fd57377f44896d0e1690473
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2021-05-26 17:18:20 +0200

    disable exception catching option set

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
     performSox.c \
     sox/local_install_dir/usr/local/lib/libsox.a \
     -I sox/src \
+    -s DISABLE_EXCEPTION_CATCHING=0 \
     -o ./wasmsox.js \
     --emrun
