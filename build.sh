4ccc621dd99ebe73d436d0a1672e67ae64f393f3
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2021-05-26 16:51:25 +0200

    new output file

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
     performSox.c \
     sox/local_install_dir/usr/local/lib/libsox.a \
     -I sox/src \
+    -o ./wasmsox.js \
     --emrun
